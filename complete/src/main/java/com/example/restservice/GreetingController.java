c3af5058893ce2b7d29b18ef4af7fc4428085513
irina.kamalova <irina.kamalova@revolut.com>
2021-12-02 09:11:19 +0000

    Adding rate limiter to the system

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.restservice;
 
+import com.google.common.util.concurrent.RateLimiter;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +12,11 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private final RateLimiter rateLimiter = RateLimiter.create(0.1);
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		rateLimiter.acquire();
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
